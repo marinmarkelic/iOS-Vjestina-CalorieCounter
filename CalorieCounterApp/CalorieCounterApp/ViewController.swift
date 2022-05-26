9ff8476f0697153e5e85b6689e974a30ed3b3039
marinmarkelic <marin.markelic.ets@gmail.com>
2022-05-26 12:00:56 +0200

    First commit

---
+++ CalorieCounterApp/CalorieCounterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalorieCounterApp
+//
+//  Created by Marin on 26.05.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
