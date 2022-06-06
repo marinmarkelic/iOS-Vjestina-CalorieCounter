4a90143d3e0692a38501eedc6c36e44f441cd933
marinmarkelic <marin.markelic.ets@gmail.com>
2022-06-06 23:49:49 +0200

    Moved collection view from WeightView to a separate class

---
+++ CalorieCounterApp/CalorieCounterApp/UserInfo/NumberCollectionView.swift
@@ -0,0 +1,88 @@
+import UIKit
+
+class NumberCollectionView: UICollectionView{
+    
+    init(){
+        let collectionViewLayout = UICollectionViewFlowLayout()
+        collectionViewLayout.scrollDirection = .horizontal
+        
+        super.init(frame: .zero, collectionViewLayout: collectionViewLayout)
+        register(WeightViewCell.self, forCellWithReuseIdentifier: WeightViewCell.reuseIdentifier)
+        dataSource = self
+        delegate = self
+        backgroundColor = .none
+        showsHorizontalScrollIndicator = false
+                         
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func scrollCollectionView(to: Int){
+        layoutIfNeeded()
+        scrollToItem(at: IndexPath(item: to, section: 0), at: .centeredHorizontally, animated: true)
+    }
+    
+    func getValue() -> Int{
+        let indexArr = indexPathsForVisibleItems.map({$0[1]})
+        let sumOfArr = indexArr.reduce(0, +)
+        
+        return sumOfArr / indexArr.count
+    }
+}
+
+extension NumberCollectionView: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 250
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WeightViewCell.reuseIdentifier, for: indexPath) as? WeightViewCell
+        else {
+            fatalError()
+        }
+        
+        cell.set(value: indexPath.row)
+        
+        return cell
+    }
+}
+
+extension NumberCollectionView: UICollectionViewDelegateFlowLayout {
+    
+    //postavlja dimenziju celija
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        
+//        let itemHeight: CGFloat
+//
+//        if indexPath.row % 5 == 0{
+//            itemHeight = CGFloat(40)
+//        }
+//        else{
+//            itemHeight = CGFloat(20)
+//        }
+        let itemHeight = CGFloat(20)
+        let itemWidth = CGFloat(35)
+        
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
+}
+
+//extension NumberCollectionView: UICollectionViewDelegate{
+//    override func didAddSubview(_ subview: UIView) {
+//        self.scrollCollectionView(to: 170)
+//
+//    }
+//}
--- CalorieCounterApp/CalorieCounterApp/UserInfo/WeightView.swift
+++ CalorieCounterApp/CalorieCounterApp/UserInfo/WeightView.swift
@@ -4,16 +4,12 @@ import SnapKit
 class WeightView: UIView{
     private var label: UILabel!
     
-    private var collectionView: UICollectionView!
-    private var collectionViewLayout: UICollectionViewFlowLayout!
-        
+    private var numberCollectionView: NumberCollectionView!
+            
     private var arrow: UIImageView!
     
     init(){
         super.init(frame: .zero)
-        
-        
-                         
                          
         buildViews()
         addConstraints()
@@ -28,30 +24,23 @@ class WeightView: UIView{
         label = UILabel()
         label.text = "Weight"
         
-        collectionViewLayout = UICollectionViewFlowLayout()
-        collectionViewLayout.scrollDirection = .horizontal
-        
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
-        collectionView.register(WeightViewCell.self, forCellWithReuseIdentifier: WeightViewCell.reuseIdentifier)
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        collectionView.backgroundColor = .none
-        collectionView.showsHorizontalScrollIndicator = false
-        
+        numberCollectionView = NumberCollectionView()
+                
         arrow = UIImageView(image: UIImage(systemName: "arrowtriangle.up.fill"))
         arrow.tintColor = .white
         
         addSubview(label)
-        addSubview(collectionView)
+        addSubview(numberCollectionView)
         addSubview(arrow)
     }
+    
     
     func addConstraints(){
         label.snp.makeConstraints{
             $0.top.leading.equalToSuperview().offset(20)
         }
         
-        collectionView.snp.makeConstraints{
+        numberCollectionView.snp.makeConstraints{
             $0.top.equalTo(label.snp.bottom).offset(10)
             $0.leading.equalToSuperview().offset(20)
             $0.trailing.equalToSuperview().offset(-20)
@@ -59,100 +48,17 @@ class WeightView: UIView{
         }
         
         arrow.snp.makeConstraints{
-            $0.top.equalTo(collectionView.snp.bottom).offset(10)
+            $0.top.equalTo(numberCollectionView.snp.bottom).offset(10)
             $0.bottom.equalToSuperview()
             $0.centerX.equalToSuperview()
         }
     }
     
-    func scrollCollectionView(to: Int){
-        collectionView.layoutIfNeeded()
-        collectionView.scrollToItem(at: IndexPath(item: to, section: 0), at: .centeredHorizontally, animated: true)
+    func scrollCollectionView(to value: Int){
+        numberCollectionView.scrollCollectionView(to: value)
     }
     
     func getValue() -> Int{
-        let indexArr = collectionView.indexPathsForVisibleItems.map({$0[1]})
-        let sumOfArr = indexArr.reduce(0, +)
-        
-        return sumOfArr / indexArr.count
-    }
-}
-
-final class SnapCenterLayout: UICollectionViewFlowLayout {
-  override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
-    guard let collectionView = collectionView else { return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity) }
-    let parent = super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
-
-    let itemSpace = itemSize.width + minimumInteritemSpacing
-    var currentItemIdx = round(collectionView.contentOffset.x / itemSpace)
-
-    // Skip to the next cell, if there is residual scrolling velocity left.
-    // This helps to prevent glitches
-    let vX = velocity.x
-    if vX > 0 {
-      currentItemIdx += 1
-    } else if vX < 0 {
-      currentItemIdx -= 1
-    }
-
-    let nearestPageOffset = currentItemIdx * itemSpace
-    return CGPoint(x: nearestPageOffset,
-                   y: parent.y)
-  }
-}
-
-extension WeightView: UICollectionViewDataSource {
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 250
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        guard
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WeightViewCell.reuseIdentifier, for: indexPath) as? WeightViewCell
-        else {
-            fatalError()
-        }
-        
-        cell.set(value: indexPath.row)
-        
-        return cell
-    }
-}
-
-extension WeightView: UICollectionViewDelegateFlowLayout {
-    
-    //postavlja dimenziju celija
-    func collectionView(
-        _ collectionView: UICollectionView,
-        layout collectionViewLayout: UICollectionViewLayout,
-        sizeForItemAt indexPath: IndexPath
-    ) -> CGSize {
-        
-//        let itemHeight: CGFloat
-//
-//        if indexPath.row % 5 == 0{
-//            itemHeight = CGFloat(40)
-//        }
-//        else{
-//            itemHeight = CGFloat(20)
-//        }
-        let itemHeight = CGFloat(20)
-        let itemWidth = CGFloat(30)
-        
-        return CGSize(width: itemWidth, height: itemHeight)
-    }
-}
-
-extension WeightView: UICollectionViewDelegate{
-    
-    override func didAddSubview(_ subview: UIView) {
-        self.scrollCollectionView(to: 170)
-
+        return numberCollectionView.getValue()
     }
 }
